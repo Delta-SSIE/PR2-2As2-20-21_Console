347bb974c0ec0396ff2837a2d2adfbce223aa424
Jan Koupil <jan.koupil@delta-skola.cz>
2020-10-02 09:09:15 +0200

    Lekce: Enum

---
+++ 005_Enum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _005_Enum
+{
+    enum Obdobi:byte { Jaro, Leto, Podzim, Zima }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Obdobi chladne = Obdobi.Podzim;
+
+            Console.WriteLine(chladne);
+            chladne++;
+            Console.WriteLine(chladne);
+            chladne++;
+            Console.WriteLine(chladne);
+
+            Obdobi horke = (Obdobi)1;
+            Console.WriteLine(horke);
+
+            Obdobi divne = (Obdobi)3;
+
+            Console.WriteLine(divne);
+            divne += 252;
+            Console.WriteLine(divne);
+            divne++;
+            Console.WriteLine(divne);
+
+            Console.WriteLine((byte)Obdobi.Jaro + (byte)Obdobi.Podzim);
+
+        }
+    }
+}
---
+++ 006_Enum_Karty/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _006_Enum_Karty
+{
+    enum Barva { Srdce, Kary, Piky, Krize, Trefy = Krize };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Barva moje = Barva.Krize;
+            Barva tvoje = Barva.Trefy;
+            if (moje == tvoje)
+                Console.WriteLine("Máme stejnou barvu");
+            else
+                Console.WriteLine("Každý máme jinou barvu");
+        }
+    }
+}
