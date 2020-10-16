bf4229078c625da70649de83621966bc51d6c765
Jan Koupil <jan.koupil@delta-skola.cz>
2020-10-16 09:32:11 +0200

    LEkce OOP 16. 10 2020 - Přidán trojúhelník

---
+++ 013-Trojuhelnik/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _013_Trojuhelnik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trojuhelnik abc = new Trojuhelnik(1, 1, 5);
+            Console.WriteLine($"Trojúhelník o stranách {abc.VratA()}, {abc.VratB()}, {abc.VratC()} má obvod {abc.Obvod()} a obsah {abc.Obsah()}.");
+        }
+    }
+}
---
+++ 013-Trojuhelnik/Trojuhelnik.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _013_Trojuhelnik
+{
+    class Trojuhelnik
+    {
+        private double _stranaA;
+        private double _stranaB;
+        private double _stranaC;
+        public Trojuhelnik(double stranaA, double stranaB, double stranaC)
+        {
+            NastavA(stranaA);
+            NastavB(stranaB);
+            NastavC(stranaC);
+        }
+        public double VratA()
+        {
+            return _stranaA;
+        }
+        public void NastavA(double hodnota)
+        {
+            if (hodnota < 0)
+                throw new System.ArgumentOutOfRangeException();
+            else
+                _stranaA = hodnota;
+        }
+        public double VratB()
+        {
+            return _stranaB;
+        }
+        public void NastavB(double hodnota)
+        {
+            if (hodnota < 0)
+                throw new System.ArgumentOutOfRangeException();
+            else
+                _stranaB = hodnota;
+        }
+        public double VratC()
+        {
+            return _stranaC;
+        }
+        public void NastavC(double hodnota)
+        {
+            if (hodnota < 0)
+                throw new System.ArgumentOutOfRangeException();
+            else
+                _stranaC = hodnota;
+        }
+        public double Obvod()
+        {
+            return _stranaA + _stranaB + _stranaC;
+        }
+        public double Obsah()
+        {
+            double s = (_stranaA + _stranaB + _stranaC) / 2;
+            return Math.Sqrt(s * (s - _stranaA) * (s - _stranaB) * (s - _stranaC));
+        }
+        private bool lzeSestavit(double a, double b, double c)
+        {
+            // vrátí true/false podle trojúhelníkové nerovnosti
+        }
+
+
+    }
+}
