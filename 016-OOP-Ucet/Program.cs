630bd93dcc0042405cc7d737ea29880891e8e2d5
Jan Koupil <jan.koupil@delta-skola.cz>
2020-11-06 08:26:48 +0100

    zaklad tridy ucet

---
+++ 016-OOP-Ucet/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _016_OOP_Ucet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ucet ucet = new Ucet("Franta");
+            Console.WriteLine($"Na účtu je {ucet.Zustatek} Kč.");
+
+            ucet.Uloz(1000);
+            Console.WriteLine($"Na účtu je {ucet.Zustatek} Kč.");
+
+            if (ucet.Vyber(70))
+            {
+                Console.WriteLine("Vybráno");
+            }
+            else
+            {
+                Console.WriteLine("Tolik na účtu není.");
+            }
+            Console.WriteLine($"Na účtu je {ucet.Zustatek} Kč.");
+
+            double castka = 70;
+            while (ucet.Vyber(castka))
+            {
+                Console.WriteLine($"Vybráno {castka} Kč, zbývá {ucet.Zustatek}");
+            }
+
+            Console.WriteLine("Došly prachy.");
+
+        }
+    }
+}
---
+++ 016-OOP-Ucet/Ucet.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _016_OOP_Ucet
+{
+    class Ucet
+    {
+        private double _zustatek;
+        private string _vlastnik;
+
+        public double Zustatek
+        {
+            get
+            {
+                return _zustatek;
+            }
+
+        }
+        public Ucet(string vlastnik)
+        {
+            _vlastnik = vlastnik;
+            _zustatek = 0;
+        }
+        public void Uloz(double castka)
+        {
+            _zustatek += castka; 
+        }
+        public bool Vyber(double castka)
+        {
+            //když na to mám
+            if (castka <= _zustatek)
+            {
+                _zustatek -= castka;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
