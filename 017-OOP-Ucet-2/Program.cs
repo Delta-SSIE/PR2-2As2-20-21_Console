04e4d50b250cf8b968769454ccc178d8f8e29e42
Jan Koupil <jan.koupil@delta-skola.cz>
2020-11-06 09:12:54 +0100

    Do účtu přidána hesla

---
+++ 017-OOP-Ucet-2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _017_OOP_Ucet_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ucet ucet = new Ucet("swordfish");
+            ucet.Uloz(1000);
+            if (ucet.Vyber(100, "swordfish"))
+            {
+                Console.WriteLine("Vybírám");
+            }
+            else
+            {
+                Console.WriteLine("Chyba při výběru. Zkontrolujte heslo.");
+            }
+
+            if (ucet.ZmenHeslo("swordfis", "NCC-1701"))
+            {
+                Console.WriteLine("Změna hesla úspěšná");
+            }
+            else
+            {
+                Console.WriteLine("Chyba při změně hesla");
+            }
+
+            if (ucet.Vyber(100, "NCC-1701")) //nepovede se - byla chyba při změně
+            {
+                Console.WriteLine("Vybírám");
+            }
+            else
+            {
+                Console.WriteLine("Chyba při výběru. Zkontrolujte heslo.");
+            }
+
+
+            if (ucet.ZmenHeslo("swordfish", "NCC-1701"))
+            {
+                Console.WriteLine("Změna hesla úspěšná");
+            }
+            else
+            {
+                Console.WriteLine("Chyba při změně hesla");
+            }
+
+            if (ucet.Vyber(100, "NCC-1701")) //povede se
+            {
+                Console.WriteLine("Vybírám");
+            }
+            else
+            {
+                Console.WriteLine("Chyba při výběru. Zkontrolujte heslo.");
+            }
+
+
+        }
+    }
+}
---
+++ 017-OOP-Ucet-2/Ucet.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _017_OOP_Ucet_2
+{
+    class Ucet
+    {
+        public double Zustatek { get; private set; }
+        private string _heslo;
+
+        public Ucet(string heslo)
+        {
+            _heslo = heslo;
+            Zustatek = 0;
+        }
+        public void Uloz(double castka)
+        {
+            Zustatek += castka; 
+        }
+        public bool Vyber(double castka, string heslo)
+        {
+            if (heslo != _heslo)
+            {
+                return false; //špatné heslo
+            }
+            //když na to mám
+            if (castka <= Zustatek)
+            {
+                Zustatek -= castka;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Urokuj(double urokovaSazba)
+        {
+            double koeficient = 1 + urokovaSazba / 100;
+            Zustatek *= koeficient;
+        }
+        public bool ZmenHeslo(string stareHeslo, string noveHeslo)
+        {
+            //když je staré heslo správně
+            if (stareHeslo == _heslo)
+            {
+                _heslo = noveHeslo;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
