8c5985be30163a2d5fcea3d5b1f37a38c4427079
Jan Koupil <jan.koupil@delta-skola.cz>
2020-11-06 08:59:35 +0100

    Přidáno dvojnásobení úrokováním

--- 016-OOP-Ucet/Program.cs
+++ 016-OOP-Ucet/Program.cs
@@ -34,8 +34,25 @@ namespace _016_OOP_Ucet
             for (int i = 0; i < 10; i++)
             {
                 ucet.Urokuj(1.7);
-                Console.WriteLine("Na účtu je {0} Kč.", ucet.Zustatek);
+                Console.WriteLine("Na účtu je {0:0.00} Kč.", ucet.Zustatek);
             }
+
+            
+            Ucet ucet2 = new Ucet("Pepa");
+            double vklad = 1;
+            double urokovaMira = 1.7;
+            ucet2.Uloz(vklad);
+            
+
+            int obdobi = 0;
+            
+            while (ucet2.Zustatek < 2 * vklad)
+            {
+                ucet2.Urokuj(urokovaMira);
+                obdobi++;
+            }
+
+            Console.WriteLine($"Zdvojnásobí se za {obdobi} období.");
 
         }
     }
