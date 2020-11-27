58e938afd966d467c7de948027d92cc07ba2b909
Jan Koupil <jan.koupil@delta-skola.cz>
2020-11-27 09:42:14 +0100

    Tvorba seznamu prvočísel

---
+++ 21-Prvocisla/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _21_Prvocisla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int max = 50;
+            List<int> prvocisla = new List<int>(); //seznam pro prvočísla
+
+            for (int cislo = 2; cislo <= max; cislo++) // zační na 2, skončí na max
+            {
+                if (Prvocisla.JePrvocislo(cislo)) //když je prvočíslo
+                    prvocisla.Add(cislo); //zapiš si ho do seznamu
+            }
+
+            Console.WriteLine(string.Join(", ", prvocisla));
+
+            // a nebo úplně jinak - vyžaduje System.Linq, zde byla chyba při hodině
+            int[] prvocisla2 = Enumerable.Range(2, max - 1).Where(x => Prvocisla.JePrvocislo(x)).ToArray();
+            Console.WriteLine(string.Join(", ", prvocisla2));
+
+        }
+    }
+}
---
+++ 21-Prvocisla/Prvocisla.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _21_Prvocisla
+{
+    class Prvocisla
+    {
+        public static bool JePrvocislo(int cislo) //testuje, jestli je prvočíslo
+        {
+            for (int zkousim = 2; zkousim < Math.Sqrt(cislo); zkousim++) //vyzkouší všechna čísla podezřelá, že by mohla být dělitelem
+            {
+                if (cislo % zkousim == 0) //zjistí zbytek po dělení
+                {
+                    return false; //našel jsem dělitele, vracím false (NENÍ prvočíslo)
+                }
+            }
+
+            return true; //vše doběhlo, nenašel jsem dělitele => dělitel není, vracím true (JE prvočíslo)
+        }
+    }
+}
