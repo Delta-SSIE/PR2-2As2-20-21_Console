cb309828f7d361d035e0dde5970c4f23e1ff96e1
Jan Koupil <jan.koupil@gmail.com>
2020-12-17 22:40:46 +0100

    Pripravena data pro bludiste - zadane slovnikem

---
+++ 24-Bludiste-Dictionary/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _24_Bludiste_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, char[]> bludiste = new Dictionary<char, char[]>();
+            bludiste.Add('A', new char[]{'E', 'M'});
+            bludiste.Add('B', new char[] { 'E', 'Š', 'T' });
+            bludiste.Add('C', new char[] { 'E', 'Ň', 'U' });
+            bludiste.Add('Č', new char[] { 'I', 'Q', 'W' });
+            bludiste.Add('D', new char[] { 'G', 'K', 'Z' });
+            bludiste.Add('Ď', new char[] { 'M', 'R', 'X' });
+            bludiste.Add('E', new char[] { 'A', 'B', 'C' });
+            bludiste.Add('Ě', new char[] { 'L', 'V', 'Y' });
+            bludiste.Add('F', new char[] { 'F', 'H', 'S' });
+            bludiste.Add('G', new char[] { 'D', 'K', 'U' });
+            bludiste.Add('H', new char[] { 'F', 'Ň', 'S' });
+            bludiste.Add('I', new char[] { 'Č', 'N', 'W' });
+            bludiste.Add('J', new char[] { 'Š', 'T', 'Ť' });
+            bludiste.Add('K', new char[] { 'D', 'G', 'Z' });
+            bludiste.Add('L', new char[] { 'Ě', 'V', 'Y' });
+            bludiste.Add('X', new char[] { 'Ď', 'O', 'R' });
+            bludiste.Add('Y', new char[] { 'Ě', 'L', 'Ř' });
+            bludiste.Add('M', new char[] { 'A', 'Ď', 'O' });
+            bludiste.Add('N', new char[] { 'I', 'P', 'Q' });
+            bludiste.Add('Ň', new char[] { 'C', 'H', 'S' });
+            bludiste.Add('O', new char[] { 'M', 'R', 'X' });
+            bludiste.Add('P', new char[] { 'N', 'U', 'Ž' });
+            bludiste.Add('Q', new char[] { 'Č', 'N', 'W' });
+            bludiste.Add('R', new char[] { 'Ď', 'O', 'X' });
+            bludiste.Add('Ř', new char[] { 'V', 'Y', 'Ž' });
+            bludiste.Add('S', new char[] { 'F', 'H', 'Ň' });
+            bludiste.Add('Š', new char[] { 'B', 'J', 'Ť' });
+            bludiste.Add('T', new char[] { 'B', 'J', 'Ť' });
+            bludiste.Add('Ť', new char[] { 'J', 'Š', 'T' });
+            bludiste.Add('U', new char[] { 'C', 'G', 'P' });
+            bludiste.Add('V', new char[] { 'Ě', 'L', 'Ř' });
+            bludiste.Add('W', new char[] { 'Č', 'I', 'Q' });
+            bludiste.Add('Z', new char[] { 'D', 'K', 'Z' });
+            bludiste.Add('Ž', new char[] { 'P', 'Ř' });
+
+
+    }
+}
