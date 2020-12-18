d0589cdf0cdfba91b5854c993bf045e83eb5b3e3
Jan Koupil <jan.koupil@delta-skola.cz>
2020-12-18 09:01:14 +0100

    Bludiste - reseni

--- 24-Bludiste-Dictionary/Program.cs
+++ 24-Bludiste-Dictionary/Program.cs
@@ -8,7 +8,7 @@ namespace _24_Bludiste_Dictionary
         static void Main(string[] args)
         {
             Dictionary<char, char[]> bludiste = new Dictionary<char, char[]>();
-            bludiste.Add('A', new char[]{'E', 'M'});
+            bludiste.Add('A', new char[] { 'E', 'M' });
             bludiste.Add('B', new char[] { 'E', 'Š', 'T' });
             bludiste.Add('C', new char[] { 'E', 'Ň', 'U' });
             bludiste.Add('Č', new char[] { 'I', 'Q', 'W' });
@@ -43,6 +43,79 @@ namespace _24_Bludiste_Dictionary
             bludiste.Add('Z', new char[] { 'D', 'K', 'Z' });
             bludiste.Add('Ž', new char[] { 'P', 'Ř' });
 
+            //piš si ke každému rozcestí, v kolikátém kroku ses sem dostal
+            Dictionary<char, byte> kroky = new Dictionary<char, byte>();
 
+            //dávej si do fronty všechna rozcestí, kde jsi ještě nebyl, abys je později mohl projít
+            Queue<char> kamJit = new Queue<char>();
+
+            char start = 'A';
+            char cil = 'Ž';
+
+            kamJit.Enqueue(start); //aby bylo odkud začít, inicializuju frontu
+            kroky[start] = 0; //na startu jsem hned
+
+            while (kamJit.Count > 0) //dokud je ještě neprozkoumaná křižovatka
+            {
+                char rozcesti = kamJit.Dequeue(); //vezmi prvni rozcesti z fronty
+                                
+                if (rozcesti == cil)
+                    break; //jsem na konci
+
+                byte krok = kroky[rozcesti]; //kolikatym krokem jsem se sem dostal
+                krok++; //do rozcesti okolo je to o krok dal
+
+                //projdi všechny odbočky
+                foreach (char pristi in bludiste[rozcesti])
+                {
+                    //když jsem tam už byl, přeskočím
+                    if (kroky.ContainsKey(pristi))
+                        continue;
+
+                    //jinak si zapíšu nové rozcestí
+                    //kroky.Add(pristi, krok); //nebo
+                    kroky[pristi] = krok;
+
+                    //a přidám odbočky do fronty
+                    kamJit.Enqueue(pristi);
+                }
+            }
+
+            if(!kroky.ContainsKey(cil))
+            {
+                Console.WriteLine("Cíl nenalezen!");
+                return;
+            }
+
+            //musím zpět od cíle hledat, kudy se tam šlo - kudy klesá uložený počet kroků
+
+            char pozice = cil;
+            List<char> cesta = new List<char>();
+            cesta.Add(cil);
+
+            while (pozice != start)
+            {
+                //kolikatym krokem se sem šlo
+                byte krok = kroky[pozice];
+
+                //projdu všechny odbočky
+                foreach (char soused in bludiste[pozice])
+                {
+                    //když najdu souseda s nižším počtem kroků, to je moje cesta
+                    if (kroky[soused] == krok - 1)
+                    {
+                        cesta.Add(soused); //zapíšu si do cesty
+                        pozice = soused; //přesunu se tam
+                        break; //ukončím hledání sousedů
+                    }
+                }
+            }
+
+            cesta.Reverse();
+
+            foreach(char misto in cesta)
+                Console.Write(misto);
+
+        }
     }
 }
